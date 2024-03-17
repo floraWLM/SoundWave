34f8d5b03603167f6a1201cf7049a10ad380237d
floraWLM <lemengwangflora@163.com>
2024-03-16 21:49:54 -0400

    Add functionality quit by key q

--- 1_wave.py
+++ 1_wave.py
@@ -4,7 +4,18 @@ import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt 
 
-CHUNK = 1024
+# quit by key q function
+def on_press(event):
+    global stream, p, END
+    if event.key == 'q':
+        plt.close()
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        END = True
+
+END = False
+CHUNK = 1024 * 2
 FORMAT = pyaudio.paInt16
 CHANNEL = 1
 RATE = 44100
@@ -14,16 +25,24 @@ p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT,channels=CHANNEL,rate=RATE, input=True,frames_per_buffer=CHUNK)
 
 # Visualize the data
+mpl.rcParams['toolbar'] = 'None'
 fig, ax = plt.subplots(figsize=(12,3))
+
+# Quit by key q
+fig.canvas.mpl_connect('key_press_event', on_press)
+
+plt.subplots_adjust(left=0.001,top=0.999,right=0.999,bottom=0.001)
+plt.get_current_fig_manager().set_window_title('Wave')
 x = np.arange(0,CHUNK)
 line, = ax.plot(x, np.random.rand(CHUNK),color='#C04851')
 ax.set_xlim(0,CHUNK-1)
 ax.set_ylim(-2**15,2**15)
 
+plt.axis('off')
 plt.ion()
 plt.show()
 
-while True:
+while END == False:
     data = stream.read(CHUNK,exception_on_overflow=False)
     data = np.frombuffer(data,dtype=np.int16)
     line.set_ydata(data)
